d12f00ff72a1e8e2fa2ef8d12a6757b0ce5be453
Teresa Hurtado <matehurgal@gmail.com>
2025-04-08 19:54:39 -0600

    Update interrupts.h

--- interrupts.h
+++ interrupts.h
@@ -1,6 +1,7 @@
 #ifndef _interrupts
 #define _interrupts
-	void EXTI_IRQHandler(void);
-	void ext_interrupt(void);
+	void EXTI1_IRQHandler(void);
 	void EXTI2_IRQHandler(void);
+	void ext_interrupt(void);
+	void ext_interrupt2(void);
 #endif
